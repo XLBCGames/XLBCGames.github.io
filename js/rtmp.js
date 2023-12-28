4bca658285963250d0e21faf493864e2524f09a1
XLBCGames <141425829+XLBCGames@users.noreply.github.com>
2023-12-28 22:00:00 +0800

    Update rtmp.js

--- js/rtmp.js
+++ js/rtmp.js
@@ -1,12 +1,17 @@
 window.onload = function() {
-  if (flvjs.isSupported()) {
-    var videoElement = document.getElementById('rtmp-player');
-    var flvPlayer = flvjs.createPlayer({
-      type: 'flv',
-      url: 'rtmp://frp-man.top:12117/live'
-    });
-    flvPlayer.attachMediaElement(videoElement);
-    flvPlayer.load();
-    flvPlayer.play();
+  var videoElement = document.getElementById('video-player');
+  var rtmpUrl = 'rtmp://frp-man.top:12117/live';
+
+  if (videoElement.canPlayType('application/x-mpegURL') !== '') {
+    // 支持 HLS
+    videoElement.src = rtmpUrl + '/playlist.m3u8';
+  } else if (videoElement.canPlayType('application/vnd.apple.mpegURL') !== '') {
+    // 支持 Safari 的 HLS
+    videoElement.src = rtmpUrl + '/playlist.m3u8';
+  } else if (videoElement.canPlayType('application/octet-stream') !== '') {
+    // 支持 RTMP
+    videoElement.src = rtmpUrl;
+  } else {
+    console.error('无法播放 RTMP 流');
   }
 };
